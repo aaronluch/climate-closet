9e37b122c1abb8f3540e8988527b4469b3da5b5f
Aaron Luciano <aluch07@yahoo.com>
2024-10-08 12:03:25 -0400

    Initial Commit

---
+++ climate-closet/climate_closetApp.swift
@@ -0,0 +1,17 @@
+//
+//  climate_closetApp.swift
+//  climate-closet
+//
+//  Created by Aaron Luciano on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct climate_closetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ climate-closet/clothing.swift
@@ -0,0 +1,22 @@
+class Clothing {
+    var name: String
+    var owned: Bool
+    var category: Category
+    var minTemp: String
+    var maxTemp: String
+    var imageUrl: String
+    
+    enum Category {
+        case top, bottom, outerwear, accessory, footwear, other
+    }
+    
+    init(name: String = "", owned: Bool = true, category: Category = .other, minTemp: String = "", 
+    maxTemp: String = "", imageUrl: String = "") {
+        self.name = name
+        self.owned = owned
+        self.category = category
+        self.minTemp = minTemp
+        self.maxTemp = maxTemp
+        self.imageUrl = imageUrl
+    }
+}
