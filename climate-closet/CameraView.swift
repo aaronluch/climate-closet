c521261e01403521139fba2e288795beeafbbadb
icordero411 <eddiesbadbreak@gmail.com>
2024-10-16 14:38:50 -0400

    Updated camera view

--- climate-closet/CameraView.swift
+++ climate-closet/CameraView.swift
@@ -1,9 +1,31 @@
 import SwiftUI
+import UIKit
 
 struct CameraView: View {
+    @State private var isImagePickerPresented: Bool = false
+    @State private var capturedImage: UIImage?
+
     var body: some View {
-        Text("this is where the camera implementation goes")
-            .navigationTitle("Camera")
+        VStack {
+            
+            Button(action: {
+                isImagePickerPresented = true
+            }) {
+                Text("Open Camera")
+            }
+            .sheet(isPresented: $isImagePickerPresented) {
+                ImagePicker(capturedImage: $capturedImage)
+            }
+
+            if let image = capturedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .cornerRadius(10)
+                    .padding()
+            }
+        }
     }
 }
 
@@ -12,4 +34,3 @@ struct CameraView_Previews: PreviewProvider {
         CameraView()
     }
 }
-
